e12dd1cfb1e66414ba4dbb7d607159b46a19178a
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-04-23 14:16:49 +0900

    Initial Commit

---
+++ StampCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StampCard
+//
+//  Created by KazukiNakano on 2020/04/23.
+//  Copyright © 2020 kazudmb. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
