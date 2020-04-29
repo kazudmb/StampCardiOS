10fe4f78743979c2c54327ad7c915e0aad9ac60b
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-04-29 22:52:32 +0900

    add DataRepository File

---
+++ StampCard/DataRepository.swift
@@ -0,0 +1,14 @@
+//
+//  DataRepository.swift
+//  StampCard
+//
+//  Created by KazukiNakano on 2020/04/29.
+//  Copyright © 2020 kazudmb. All rights reserved.
+//
+
+import Foundation
+
+import Firebase
+
+var auth: Auth!
+var numberOfVisits = 0
