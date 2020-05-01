de3f365c3a1bbd8a0a0093de4d0cd411f6877d38
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-05-01 18:57:46 +0900

    add ActionSheet (1.Change Email, 2.Change Password, 3.Logout)

--- StampCard/AccountInfoView.swift
+++ StampCard/AccountInfoView.swift
@@ -14,7 +14,28 @@ struct AccountInfoView: View {
     
     @State var email = "test@gmail.com"
     @State private var isShowAlert = false
+    @State private var isShowActionSheet = false
     @Binding var isShowAccountInfoView: Bool
+    
+    var actionSheet: ActionSheet {
+        ActionSheet(title: Text("選択してください"),buttons: [
+            .default(Text("メールアドレス変更"),action: {}),
+            .default(Text("パスワード変更"),action: {}),
+            .destructive(Text("ログアウト"),action: {self.isShowAlert.toggle()}),
+            .cancel()
+        ])
+    }
+    
+    var alert: Alert{
+        Alert(title: Text("ログアウトしてもよろしいですか？"),
+              primaryButton:
+            .cancel(Text("キャンセル"),action: {print("cancel tapped")}),
+              secondaryButton:
+            .destructive(Text("ログアウト"),
+                         action: {
+                            self.Logout()
+                            print("logout tapped")}))
+    }
     
     var body: some View {
         HStack {
@@ -30,20 +51,14 @@ struct AccountInfoView: View {
         .navigationBarTitle(Text("アカウント情報"), displayMode:.inline)
         .navigationBarItems(trailing:
             Button(action: {
-                self.isShowAlert.toggle()
+                self.isShowActionSheet.toggle()
             }) {
-                Text("ログアウト")
-        })
-            .alert(isPresented: $isShowAlert) {
-                Alert(title: Text("ログアウトしてもよろしいですか？"),
-                      primaryButton:
-                    .cancel(Text("キャンセル"),action: {print("cancel tapped")}),
-                      secondaryButton:
-                    .destructive(Text("ログアウト"),
-                                 action: {
-                                    self.Logout()
-                                    print("logout tapped")}))
-        }
+                Image(systemName: "ellipsis.circle")
+            }
+            .actionSheet(isPresented: $isShowActionSheet, content: {self.actionSheet})
+            .alert(isPresented: $isShowAlert, content: {self.alert})
+        )
+            .foregroundColor(Color.black)
     }
     
     func Logout(){
