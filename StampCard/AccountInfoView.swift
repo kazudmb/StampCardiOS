f6e6fbd2e8b94b85b175011c09426f3d79f85ab7
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-05-01 13:01:25 +0900

    add Dialog of Logout Text Tapped

--- StampCard/AccountInfoView.swift
+++ StampCard/AccountInfoView.swift
@@ -13,6 +13,7 @@ import Firebase
 struct AccountInfoView: View {
     
     @State var email = "test@gmail.com"
+    @State private var isShowAlert = false
     @Binding var isShowAccountInfoView: Bool
     
     var body: some View {
@@ -29,10 +30,20 @@ struct AccountInfoView: View {
         .navigationBarTitle(Text("アカウント情報"), displayMode:.inline)
         .navigationBarItems(trailing:
             Button(action: {
-                self.Logout()
+                self.isShowAlert.toggle()
             }) {
                 Text("ログアウト")
         })
+            .alert(isPresented: $isShowAlert) {
+                Alert(title: Text("ログアウトしてもよろしいですか？"),
+                      primaryButton:
+                    .cancel(Text("キャンセル"),action: {print("cancel tapped")}),
+                      secondaryButton:
+                    .destructive(Text("ログアウト"),
+                                 action: {
+                                    self.Logout()
+                                    print("logout tapped")}))
+        }
     }
     
     func Logout(){
